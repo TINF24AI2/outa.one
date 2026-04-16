c015074c233668cccc292ad07313825a5d1e1de5
fowl-ow <80642246+fowl-ow@users.noreply.github.com>
2026-04-16 19:46:10 +0200

    chore: setup biome formatter and linter (#2)
    * chore: setup nix and direnv
    
    * chore: add and setup biome
    
    * refactor: format

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,13 @@
-import type { User, Session } from 'better-auth/minimal';
+import type { User, Session } from "better-auth/minimal";
 
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
 	namespace App {
-		interface Locals { user?: User; session?: Session }
+		interface Locals {
+			user?: User;
+			session?: Session;
+		}
 
 		// interface Error {}
 		// interface PageData {}
