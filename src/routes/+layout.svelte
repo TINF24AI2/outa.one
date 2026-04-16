b2f40c8e82d592df017af9f342f3fba41703603e
fowl-ow <80642246+fowl-ow@users.noreply.github.com>
2026-04-16 19:28:54 +0200

    refactor: format

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	import type { Pathname } from '$app/types';
-	import { resolve } from '$app/paths';
-	import { page } from '$app/state';
-	import { locales, localizeHref } from '$lib/paraglide/runtime';
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+import type { Pathname } from "$app/types";
+import { resolve } from "$app/paths";
+import { page } from "$app/state";
+import { locales, localizeHref } from "$lib/paraglide/runtime";
+import "./layout.css";
+import favicon from "$lib/assets/favicon.svg";
 
-	let { children } = $props();
+let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
