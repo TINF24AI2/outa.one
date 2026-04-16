b4ffc97ed7475fa3249ab0a5109eda1ebc2c64a6
fowl-ow <80642246+fowl-ow@users.noreply.github.com>
2026-04-16 19:21:35 +0200

    chore: setup nix and direnv

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "outa.one";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              pkg-config
+              nodejs_24
+              pnpm
+            ];
+          };
+      }
+    );
+}
