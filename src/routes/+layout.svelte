38b5eea6d0c046538fba4b6d581810ece9ac7f69
zFlxw <hello@flxw.dev>
2026-04-17 10:42:03 +0200

    feat: setup better auth schema

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-import type { Pathname } from "$app/types";
-import { resolve } from "$app/paths";
-import { page } from "$app/state";
-import { locales, localizeHref } from "$lib/paraglide/runtime";
-import "./layout.css";
-import favicon from "$lib/assets/favicon.svg";
+import type { Pathname } from '$app/types';
+import { resolve } from '$app/paths';
+import { page } from '$app/state';
+import { locales, localizeHref } from '$lib/paraglide/runtime';
+import './layout.css';
+import favicon from '$lib/assets/favicon.svg';
 
 let { children } = $props();
 </script>
