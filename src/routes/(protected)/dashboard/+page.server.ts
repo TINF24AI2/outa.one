3a79566d2a8a0c60a40089ca760190b6353b86bd
zFlxw <hello@flxw.dev>
2026-05-08 02:21:48 +0200

    fix: re-add signOut form action

---
+++ src/routes/(protected)/dashboard/+page.server.ts
@@ -0,0 +1,9 @@
+import { auth } from "$lib/server/auth";
+import { redirect, type Actions } from "@sveltejs/kit";
+
+export const actions: Actions = {
+  signOut: async (event) => {
+    await auth.api.signOut({ headers: event.request.headers });
+    redirect(302, '/login');
+  }
+};
