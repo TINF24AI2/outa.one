c35bb223064db94def7d62fa54cb4a9f0bd66dd6
zFlxw <hello@flxw.dev>
2026-05-21 18:28:46 +0200

    fix: broken artifacts on merge

--- src/lib/components/app/product-list.svelte
+++ src/lib/components/app/product-list.svelte
@@ -1,26 +1,25 @@
 <script lang="ts">
-import * as Table from '$lib/components/ui/table/index.js';
-import * as HoverCard from '$lib/components/ui/hover-card/index.js';
+  import { KeyIcon, Trash2, UploadIcon } from "@lucide/svelte";
 
-import { Key, KeyIcon, SquarePen, Trash2, Upload, UploadIcon } from '@lucide/svelte';
-import { request } from 'http';
-import EditProduct from './edit-product.svelte';
+  import * as Table from "$lib/components/ui/table/index.js";
 
-const products = [
-  {
-    productName: 'Adobe',
-    description: 'Hier könnte ihre Beschreibung stehen!',
-    requestApproval: true,
-    licenceType: 'Subscription',
-    totalKeys: 2,
-    keysUsed: 1,
-  },
-];
+  import EditProduct from "./edit-product.svelte";
 
-// placeholder for connection for database
-// let products = getProducts();
+  const products = [
+    {
+      productName: "Adobe",
+      description: "Hier könnte ihre Beschreibung stehen!",
+      requestApproval: true,
+      licenceType: "Subscription",
+      totalKeys: 2,
+      keysUsed: 1,
+    },
+  ];
 
-/* 
+  // placeholder for connection for database
+  // let products = getProducts();
+
+  /* 
     functions to implement:
     toLicenceView() => Direct link to the license view filter by this product
     toLicenceUpload() => Direct link to the license view with the upload license keys pop-up open
@@ -30,34 +29,32 @@ const products = [
 </script>
 
 <Table.Root class="m-1">
-    <Table.Header class="bg-gray-100">
-        <Table.Row>
-            <Table.Head>Product Name</Table.Head>
-            <Table.Head>Licence Type</Table.Head>
-            <Table.Head>Total Keys</Table.Head>
-            <Table.Head>Keys Used</Table.Head>
-            <Table.Head>Keys Available</Table.Head>
-            <Table.Head>Actions</Table.Head>
-        </Table.Row>
-    </Table.Header>
-    <Table.Body>
-        {#each products as product (product)}
-            <Table.Row>
-                <Table.Cell class="font-medium"
-                    >{product.productName}</Table.Cell
-                >
-                <Table.Cell>{product.licenceType}</Table.Cell>
-                <Table.Cell>{product.totalKeys}</Table.Cell>
-                <Table.Cell>{product.keysUsed}</Table.Cell>
-                <Table.Cell>{product.totalKeys - product.keysUsed}</Table.Cell>
+  <Table.Header class="bg-gray-100">
+    <Table.Row>
+      <Table.Head>Product Name</Table.Head>
+      <Table.Head>Licence Type</Table.Head>
+      <Table.Head>Total Keys</Table.Head>
+      <Table.Head>Keys Used</Table.Head>
+      <Table.Head>Keys Available</Table.Head>
+      <Table.Head>Actions</Table.Head>
+    </Table.Row>
+  </Table.Header>
+  <Table.Body>
+    {#each products as product (product)}
+      <Table.Row>
+        <Table.Cell class="font-medium">{product.productName}</Table.Cell>
+        <Table.Cell>{product.licenceType}</Table.Cell>
+        <Table.Cell>{product.totalKeys}</Table.Cell>
+        <Table.Cell>{product.keysUsed}</Table.Cell>
+        <Table.Cell>{product.totalKeys - product.keysUsed}</Table.Cell>
 
-                <Table.Cell>
-                    <KeyIcon class="inline-flex m-auto"></KeyIcon>
-                    <UploadIcon class="inline-flex m-auto"></UploadIcon>
-                    <EditProduct></EditProduct>
-                    <Trash2 class="inline-flex m-auto" color="red"></Trash2>
-                </Table.Cell>
-            </Table.Row>
-        {/each}
-    </Table.Body>
+        <Table.Cell>
+          <KeyIcon class="m-auto inline-flex"></KeyIcon>
+          <UploadIcon class="m-auto inline-flex"></UploadIcon>
+          <EditProduct></EditProduct>
+          <Trash2 class="m-auto inline-flex" color="red"></Trash2>
+        </Table.Cell>
+      </Table.Row>
+    {/each}
+  </Table.Body>
 </Table.Root>
